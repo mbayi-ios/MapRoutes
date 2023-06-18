de1b7c6851fb90f6d5fd1f2a8c8f48948feb21ad
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-06-18 10:16:20 +0300

    Initial Commit

---
+++ MapRoutes/MapRoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapRoutesApp.swift
+//  MapRoutes
+//
+//  Created by Amby on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapRoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
