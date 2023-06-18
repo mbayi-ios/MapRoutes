24acb2067af80d826e88cfd7c33247e019330d11
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-06-18 10:55:22 +0300

    permission denied alert, redirecting user to settings

--- MapRoutes/View/Home.swift
+++ MapRoutes/View/Home.swift
@@ -15,6 +15,11 @@ struct Home: View {
             locationManager.delegate = mapData
             locationManager.requestWhenInUseAuthorization()
         })
+        .alert(isPresented: $mapData.permissionDenied, content: {
+            Alert(title: Text("Permission Denied"), message: Text("Please Enable Permission in App Settings"), dismissButton: .default(Text("Goto Settings"), action: {
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            }))
+        })
     }
 }
 
